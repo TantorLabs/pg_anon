6730ec9b85989a7b35e24e30a8d818d43fac320c
Max Ibragimov <maxim.ibragimov@tantorlabs.ru>
2024-09-27 13:44:19 +0500

    [TTDB-450] Adding DEBUG logs for scan

--- rest_api/callbacks.py
+++ rest_api/callbacks.py
@@ -9,22 +9,25 @@ from rest_api.utils import read_dictionary_contents
 
 
 def send_webhook(url: str, response_body: BaseModel):
-    print(response_body.model_dump(by_alias=True))
+    print(f'Send webhook on {url} with requst:{response_body.model_dump(by_alias=True)}')
     response = httpx.post(
         url=url,
         json=response_body.model_dump(by_alias=True),
         verify=False
     )
-    print(response.status_code)
+    print(f'Webhook response code: {response.status_code}')
 
 
 async def scan_callback(request: ScanRequest):
+    print("[DEBUG] Run scan callback")
     try:
+        print("[DEBUG] Run init")
         init_runner = InitRunner(request)
         await init_runner.run()
 
         scan_runner = ScanRunner(request)
 
+        print("[DEBUG] Send RUN webhook")
         send_webhook(
             url=request.webhook_status_url,
             response_body=ScanStatusResponse(
@@ -33,14 +36,21 @@ async def scan_callback(request: ScanRequest):
             )
         )
 
+        print("[DEBUG] Run SCAN operation")
         await scan_runner.run()
 
+        print("[DEBUG] SCAN completed - OK")
         sens_dict_contents = read_dictionary_contents(scan_runner.output_sens_dict_file_name)
+        print("[DEBUG] Read sens_dict_contents")
         no_sens_dict_contents = None
         if scan_runner.output_no_sens_dict_file_name:
+            print("[DEBUG] Read no_sens_dict_contents")
             no_sens_dict_contents = read_dictionary_contents(scan_runner.output_no_sens_dict_file_name)
+        print("[DEBUG] Complete main operation")
     except Exception as ex:
+        print("[DEBUG] SCAN completed - FAIL")
         print(ex)
+        print("[DEBUG] Send ERROR webhook")
         send_webhook(
             url=request.webhook_status_url,
             response_body=ScanStatusResponse(
@@ -50,6 +60,7 @@ async def scan_callback(request: ScanRequest):
         )
         return
 
+    print("[DEBUG] Send COMPLETE webhook")
     send_webhook(
         url=request.webhook_status_url,
         response_body=ScanStatusResponse(
