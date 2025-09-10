111d2559ef4831b27624899b5a335acd0b45170c
max-ibragimow <107977990+max-ibragimow@users.noreply.github.com>
2025-09-10 15:44:37 +0500

    [TTDB-1025] Added custom default anonymization function (#47)
    - Added custom default anonymization function for section `func` of meta-dict
    - Added test for default anonymization function
    
    Co-authored-by: Max Ibragimov <maxim.ibragimov@tantorlabs.ru>

---
+++ tests/expected_results/test_prepared_sens_dict_result_default_func_expected.py
@@ -0,0 +1,108 @@
+{
+    "dictionary": [
+        {
+            "schema": "_SCHM.$complex#имя;@&* a'",
+            "table": "_TBL.$complex#имя;@&* a'",
+            "fields": {
+                "_FLD.$complex#имя;@&* a'": "anon_funcs.digest(\"_FLD.$complex#имя;@&* a'\", 'by_default_func', 'sha256')",
+                "fld_key": "anon_funcs.digest(\"fld_key\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "public",
+            "table": "key_value",
+            "fields": {
+                "fld_value": "anon_funcs.digest(\"fld_value\", 'by_default_func', 'sha256')",
+                "fld_key": "anon_funcs.digest(\"fld_key\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "public",
+            "table": "tbl_100",
+            "fields": {
+                "другое_поле": "anon_funcs.digest(\"другое_поле\", 'by_default_func', 'sha256')",
+                "amount": "anon_funcs.digest(\"amount\", 'by_default_func', 'sha256')",
+                "имя_поля": "anon_funcs.digest(\"имя_поля\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_mask_ext_exclude_2",
+            "table": "other_ext_tbl_2",
+            "fields": {
+                "val_2": "anon_funcs.digest(\"val_2\", 'by_default_func', 'sha256')",
+                "val_1": "anon_funcs.digest(\"val_1\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "_SCHM.$complex#имя;@&* a'",
+            "table": "_TBL.$complex#имя;@&* a'3",
+            "fields": {
+                "_FLD.$complex#имя;@&* a'": "anon_funcs.digest(\"_FLD.$complex#имя;@&* a'\", 'by_default_func', 'sha256')",
+                "fld_key": "anon_funcs.digest(\"fld_key\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_customer",
+            "table": "customer_manager",
+            "fields": {
+                "phone": "anon_funcs.digest(\"phone\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_mask_ext_exclude_2",
+            "table": "card_numbers",
+            "fields": {
+                "val": "anon_funcs.digest(\"val\", 'by_default_func', 'sha256')",
+                "другое_поле": "anon_funcs.digest(\"другое_поле\", 'by_default_func', 'sha256')",
+                "usd": "anon_funcs.digest(\"usd\", 'by_default_func', 'sha256')",
+                "имя_поля": "anon_funcs.digest(\"имя_поля\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "_SCHM.$complex#имя;@&* a'",
+            "table": "_TBL.$complex#имя;@&* a'2",
+            "fields": {
+                "_FLD.$complex#имя;@&* a'": "anon_funcs.digest(\"_FLD.$complex#имя;@&* a'\", 'by_default_func', 'sha256')",
+                "fld_key": "anon_funcs.digest(\"fld_key\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_other_3",
+            "table": "some_tbl",
+            "fields": {
+                "field_type_int8": "anon_funcs.digest(\"field_type_int8\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_customer",
+            "table": "customer_company",
+            "fields": {
+                "phone": "anon_funcs.digest(\"phone\", 'by_default_func', 'sha256')",
+                "inn": "anon_funcs.digest(\"inn\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "public",
+            "table": "contracts",
+            "fields": {
+                "amount": "anon_funcs.digest(\"amount\", 'by_default_func', 'sha256')",
+                "contract_expires": "anon_funcs.digest(\"contract_expires\", 'by_default_func', 'sha256')",
+                "details": "anon_funcs.digest(\"details\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "public",
+            "table": "inn_info",
+            "fields": {
+                "inn": "anon_funcs.digest(\"inn\", 'by_default_func', 'sha256')"
+            }
+        },
+        {
+            "schema": "schm_other_2",
+            "table": "tbl_test_anon_functions",
+            "fields": {
+                "fld_5_email": "anon_funcs.digest(\"fld_5_email\", 'by_default_func', 'sha256')"
+            }
+        }
+    ]
+}
---
+++ tests/input_dict/test_meta_dict_default_func.py
@@ -0,0 +1,60 @@
+{
+	"field": {  # must be anonymized without scanning
+		"rules": [
+			"^fld_5_em",
+			"^amount",
+			"details$",
+			"contract_expires$",
+			"inn$"
+		],
+		"constants": [
+			"usd",
+			"имя_поля"
+		]
+	},
+	"skip_rules": [
+		{
+			"schema": "schm_mask_ext_exclude_2",
+			"table": "card_numbers",  # Optional. If no "table" then whole schema will be skipped
+			"fields": ["val_skip"]  # Optional. If no "fields" then whole table will be skipped
+		}
+	],
+	"data_regex": {
+		"rules": [
+			r"""[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9-]+(\.[A-Za-z]{2,})+""",  # email
+			r"^(7?\d{10})$",  # phone 7XXXXXXXXXX
+			r"^other_ext_tbl_text",  # catch "schm_mask_ext_exclude_2.other_ext_tbl_2"
+			r"""[0-9]{3}-[0-9]{2}-[0-9]{4}""",  # social Security numbers "nnn-nn-nnnn"
+			r"""\b[0-9A-Z]{3}([^ 0-9A-Z]|\s)?[0-9]{4}\b""",  # license plate numbers aaa-nnnn
+			r"""^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$""",  # IPV4 addresses
+			r"""^([1][12]|[0]?[1-9])[\/-]([3][01]|[12]\d|[0]?[1-9])[\/-](\d{4}|\d{2})$""",  # Dates in MM/DD/YYYY format
+			# MasterCard numbers 5258704108753590
+			r"""^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$""",
+			# Visa card numbers 4563-7568-5698-4587
+			r"""\b([4]\d{3}[\s]\d{4}[\s]\d{4}[\s]\d{4}|[4]\d{3}[-]\d{4}[-]\d{4}[-]\d{4}|[4]\d{3}[.]\d{4}[.]\d{4}[.]\d{4}|[4]\d{3}\d{4}\d{4}\d{4})\b""",
+			# Any card number
+			r"""[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}""",
+			# URLs
+			r"""(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()]+|\(([^\s()]+|(\([^\s()]+\)))*\))+(?:\(([^\s()]+|(\([^\s()]+\)))*\)|[^\s`!()\[\]{};:'".,?«»“”‘’]))""",
+			r"""[0-9]{2}-[0-9]{7}"""  # INN from 1c
+		]
+	},
+	"data_const": {
+		"constants": [
+			"account",
+			"email",
+			"слово",
+			"сергей"
+		]
+	},
+	"sens_pg_types": [
+		"text",
+		"integer",
+		"bigint",
+		"character",
+		"json"
+	],
+	"funcs": {
+		"default": "anon_funcs.digest(\"%s\", 'by_default_func', 'sha256')",
+	}
+}
